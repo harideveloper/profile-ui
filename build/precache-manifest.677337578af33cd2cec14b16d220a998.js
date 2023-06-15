f0e62e8e5a5caabd1ece7fb1515bc9e85917b29a
harideveloper <hariprasad.sundharesan@gmail>
2023-06-15 11:38:00 +0100

    CV to Storage

---
+++ build/precache-manifest.677337578af33cd2cec14b16d220a998.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2006b3b38f1ce19c0ba",
+    "url": "/profile-ui/static/css/main.923e1d6a.chunk.css"
+  },
+  {
+    "revision": "f2006b3b38f1ce19c0ba",
+    "url": "/profile-ui/static/js/main.f2006b3b.chunk.js"
+  },
+  {
+    "revision": "12d24fe43865133f2f3d",
+    "url": "/profile-ui/static/js/1.12d24fe4.chunk.js"
+  },
+  {
+    "revision": "1bbdab1ed27e76485d4f",
+    "url": "/profile-ui/static/js/runtime~main.1bbdab1e.js"
+  },
+  {
+    "revision": "5cc72fd79433fdbd82d02e418d2c8877",
+    "url": "/profile-ui/index.html"
+  }
+];
