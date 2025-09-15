d86194b547a7240b9037fa3bf08328d66b2c1a5f
harideveloper <hariprasad.sundharesan@gmail>
2025-09-15 16:51:23 +0100

    content update

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import {BsLinkedin} from 'react-icons/bs'
-import {AiFillTwitterSquare,AiFillFacebook,AiFillInstagram,AiFillYoutube,AiFillGithub} from 'react-icons/ai'
+import {AiFillTwitterSquare,AiFillFacebook,AiFillInstagram,AiFillMediumSquare,AiFillGithub} from 'react-icons/ai'
 import {FiUsers,FiUmbrella,FiCoffee,FiBookOpen} from 'react-icons/fi'
 import {FaAward} from 'react-icons/fa'
 
@@ -46,19 +46,20 @@ export const certification = [
 export const socialmedia = [
     { name : "linkedIn", link :"https://www.linkedin.com/in/hariprasad-sundaresan-1bb6101b0/", icon : <BsLinkedin/>},
     { name : "github", link :"https://github.com/harideveloper", icon : <AiFillGithub/>},
+    { name : "medium", link :"https://medium.com/@hariprasad.sundharesan", icon : <AiFillMediumSquare/>},
     { name : "twitter", link :"https://twitter.com", icon : <AiFillTwitterSquare/>},
     { name : "facebook", link :"https://www.facebook.com/hari.p.sundaresan", icon : <AiFillFacebook/>},
     { name : "instagram", link :"https://www.instagram.com/", icon : <AiFillInstagram/>},
-    { name : "youtube", link :"https://www.youtube.com/", icon : <AiFillYoutube/>},
   ];
 
 export const ethics = [
-    { id:'1', name : 'Open to discussion', desc : 'Share and seek ideas', icon:<FiBookOpen className='ethics__icon'/>},
-    { id:'2',name : 'Learning Mindset ', desc : 'Best trait to not be layed off',icon:<FaAward className='ethics__icon'/>},
-    { id:'3',name : 'Actively Involved', desc : 'Listening is the best trait',icon:<FiUsers className='ethics__icon'/>},
-    { id:'4',name : 'Effective Planning', desc : 'Good way to avoid working on weekends',icon:<FiUmbrella className='ethics__icon'/>},
-    { id:'5',name : 'Trust', desc : 'Earn the trust and the rest follows', icon:<FiCoffee className='ethics__icon'/>}
-  ];
+  { id:'1', name: 'Open to discussion', desc: 'Ideas welcome—no thought too weird!', icon:<FiBookOpen className='ethics__icon'/>},
+  { id:'2', name: 'Learning Mindset', desc: 'Keep learning… or risk being outsmarted by a script!', icon:<FaAward className='ethics__icon'/>},
+  { id:'3', name: 'Actively Involved', desc: 'Listen, nod, and occasionally drop the genius idea', icon:<FiUsers className='ethics__icon'/>},
+  { id:'4', name: 'Effective Planning', desc: 'Plan smart so weekends stay sacred', icon:<FiUmbrella className='ethics__icon'/>},
+  { id:'5', name: 'Trust', desc: 'Earn it, sip coffee, repeat', icon:<FiCoffee className='ethics__icon'/>}
+];
+
 
 export const footer_apps = [
   { id : "1", name : "facebook", link :"https://www.facebook.com/hari.p.sundaresan", icon : <FaFacebookF/>},
